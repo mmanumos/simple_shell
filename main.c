9c54ea16cb3aa41cf34b7b4aaf1d12564514849e
mmanumos <mosqueramanuel5@gmail.com>
2019-11-28 00:52:17 +0000

    [Fix] The order to strtok is changed to don't alter the execve modified:   main.c

--- main.c
+++ main.c
@@ -5,21 +5,24 @@ int main(void)
         int cnt = 0, status;
         size_t s_line = 0;
         ssize_t r_getline;
-        char *c_line = NULL, *token = NULL, **argv, *path;
+        char *c_line = NULL,*c_line2 = NULL, *token = NULL, **argv, *path;
 
 	while(1)
         {
                 write(STDOUT_FILENO, "#cisfun$ ", 10);
                 if((r_getline = getline(&c_line, &s_line, stdin)) != -1)
                 {
-                        path = verify_path(strtok(c_line, " \n\t"));
                         token = strtok(c_line, " \n\t");
+
                         argv = malloc(r_getline * sizeof(char *));
                         for (cnt = 0; token; cnt++)
                         {
                                 argv[cnt] = token;
                                 token = strtok(NULL, " \n\t");
                         }
+
+			path = verify_path(argv[0]);
+
                         if (fork() == 0)
                         {
                                 execute(argv, path);
