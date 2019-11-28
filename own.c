95d6a37443fe625dd7430049425babad9d6e7cfe
mmanumos <mosqueramanuel5@gmail.com>
2019-11-28 03:30:10 +0000

    [Feature] Setting styles for Betty modified:   own.c

--- own.c
+++ own.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * *_strcpy - This is a function prototype
  * @dest: This variable recive the value of the function main
@@ -9,34 +10,42 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-        int cnt1 = 0;
-        int cnt2;
-        while (src[cnt1] != '\0')
-        {
-                cnt1++;
-        }
-        for (cnt2 = 0; cnt2 <= cnt1; cnt2++)
-        {
-                dest[cnt2] = src[cnt2];
-        }
-        return (dest);
+	int cnt1 = 0;
+	int cnt2;
+
+	while (src[cnt1] != '\0')
+	{
+		cnt1++;
+	}
+	for (cnt2 = 0; cnt2 <= cnt1; cnt2++)
+	{
+		dest[cnt2] = src[cnt2];
+	}
+	return (dest);
 }
+
+
 /**
  * _strlen - This is a function prototype
- * @s: This variable recive the value of the function main
+ * @str: This variable recive the value of the function main
  * Description: Function that returns the length of a string
  * section header: Section description
  * Return: Returns the counter value to the main function
  */
+
 int _strlen(char *str)
 {
-        int cnt = 0;
-        while (str[cnt] != '\0')
-        {
-                cnt++;
-        }
-        return (cnt);
+	int cnt;
+
+	cnt = 0;
+	while (str[cnt] != '\0')
+	{
+		cnt++;
+	}
+	return (cnt);
 }
+
+
 /**
  * *_strcat - This is a function prototype
  * @dest: This variable recive the value of the function main
@@ -45,23 +54,27 @@ int _strlen(char *str)
  * section header: Section description
  * Return: Returns the counter value to the main function
  */
+
 char *_strcat(char *dest, char *src)
 {
-        int cnt1 = 0;
-        int cnt2 = 0;
-        while (dest[cnt1] != '\0')
-        {
-                cnt1++;
-        }
-        while (src[cnt2] != '\0')
-        {
-                dest[cnt1] = src[cnt2];
-                cnt1++;
-                cnt2++;
-                dest[cnt1 + 1] = '\0';
-        }
-        return (dest);
+	int cnt1 = 0;
+	int cnt2 = 0;
+
+	while (dest[cnt1] != '\0')
+	{
+		cnt1++;
+	}
+	while (src[cnt2] != '\0')
+	{
+		dest[cnt1] = src[cnt2];
+		cnt1++;
+		cnt2++;
+		dest[cnt1 + 1] = '\0';
+	}
+	return (dest);
 }
+
+
 /**
  * *_strdup - This is a function prototype
  * @str: This variable recive the value of the auxiliary function _get_env
@@ -69,28 +82,31 @@ char *_strcat(char *dest, char *src)
  * section header: Section description
  * Return: A pointer to the new string str_copy
  */
+
 char *_strdup(char *str)
 {
-        int cnt1 = 0;
-        int cnt2 = 0;
-        char *str_copy;
-        if (str == NULL)
-        {
-                return ('\0');
-        }
-        while (str[cnt1] != '\0')
-        {
-                cnt1++;
-        }
-        str_copy = malloc((sizeof(char) * cnt1) + 1);
-        if (str_copy == NULL)
-        {
-                return ('\0');
-        }
-        while (cnt2 < cnt1)
-        {
-                str_copy[cnt2] = str[cnt2];
-                cnt2++;
-        }
-        return (str_copy);
+	int cnt1 = 0;
+	int cnt2 = 0;
+	char *str_copy;
+
+	if (str == NULL)
+	{
+		return ('\0');
+	}
+
+	while (str[cnt1] != '\0')
+	{
+		cnt1++;
+	}
+	str_copy = malloc((sizeof(char) * cnt1) + 1);
+	if (str_copy == NULL)
+	{
+		return ('\0');
+	}
+	while (cnt2 < cnt1)
+	{
+		str_copy[cnt2] = str[cnt2];
+		cnt2++;
+	}
+	return (str_copy);
 }
